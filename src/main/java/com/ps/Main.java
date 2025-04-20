dbe9a571ba2199c0cdbea9b40e58a6a9900fe308
Sheik siphat <notsheiksiphat2003@gmail.com>
2025-04-20 17:11:55 -0400

    started using switch case to keep the library in a  loop

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -28,62 +28,131 @@ public class Main {
             new Book(20, "ISBN-00019", "The Book Thief", false, "")
 
     };
-
-
     public static void displayHomeScreen(){
+
+
+        while(true){
+            mainMenu();
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice){
+                case 1:
+                    int proceedToCheckout = 0;
+                    while (proceedToCheckout != 2) {
+                        showAvailableBooks(inventory);
+
+                        System.out.println("\n");
+                        System.out.println("Would you like to checkout any of the books above? ");
+                        System.out.println("(Press 1 to proceed to checkout or 3 to exit back to HomeScreen)");
+
+                        proceedToCheckout = scanner.nextInt();
+                        if (proceedToCheckout == 1) {
+
+                            System.out.println("Please enter your name for the record: ");
+                            String userName = scanner.nextLine();
+
+                            System.out.println("Please enter the Id of the book you'd like the checkout: ");
+                            int checkoutBookID = scanner.nextInt();
+                            for (int i = 0; i < inventory.length; i++) {
+                                if (inventory[i].getId() == checkoutBookID) {
+                                    inventory[i].setCheckedOutTo(userName);
+                                    inventory[i].setCheckedOut(true);
+                                    System.out.println("The user " + userName + " has successfully checkout the following book: " + inventory[i].getTitle());
+
+                                }
+                            }
+                        } else if (proceedToCheckout == 3) {
+                            break;
+                        }
+                    }
+                case 2:
+                    showCheckedOutBook(inventory);
+                    System.out.println( "If you would like to check in a book please press C");
+                    System.out.println(" If you want to go Home press X");
+
+                    scanner.nextLine();
+                    String checkoutOrHome = scanner.nextLine();
+
+                    if (checkoutOrHome.equals("C")){
+                        System.out.println("Please enter the ID of the book you want to check in: ");
+                        int checkInUserId = scanner.nextInt();
+
+                        checkInBook(inventory,checkInUserId);
+                        showCheckedOutBook(inventory);
+                    } else if (checkoutOrHome.equals("X")) {
+                        break;
+                    }
+                case 3:
+                    mainMenu();
+                case 4:
+                    System.out.println("You have chosen to exit the application Good Bye");
+                    return;
+                default:
+                    System.out.println("Invalid");
+                    break;
+            }
+        }
+
+
+
+
+    }
+
+    public static void mainMenu(){
 
         System.out.println("Welcome to the community library home screen please choose one of the options: ");
         System.out.println("1) Show Available Books");
         System.out.println("2) Show Checked out Books");
-        System.out.println("3) Exit the application");
+        System.out.println("4) Exit the application");
 
-        int userInput = scanner.nextInt();
-
-        if (userInput == 1 ) {
-    int proceedToCheckout = 0;
-    while (proceedToCheckout != 2) {
-        showAvailableBooks(inventory);
-
-        System.out.println("\n");
-        System.out.println("Would you like to checkout any of the books above? ");
-        System.out.println("(Press 1 to proceed to checkout or 2 to exit back to HomeScreen)");
-
-        proceedToCheckout = scanner.nextInt();
-        if (proceedToCheckout == 1) {
-
-            System.out.println("Please enter your name for the record: ");
-            String userName = scanner.nextLine();
-
-            System.out.println("Please enter the Id of the book you'd like the checkout: ");
-            int checkoutBookID = scanner.nextInt();
-            for (int i = 0; i < inventory.length; i++) {
-                if (inventory[i].getId() == checkoutBookID) {
-                    inventory[i].setCheckedOutTo(userName);
-                    inventory[i].setCheckedOut(true);
-                    System.out.println("The user " + userName + " has successfully checkout the following book: " + inventory[i].getTitle());
-
-                }
-            }
-        }
-    }
-
-    }
-    if (userInput == 2 ){
-     showCheckedOutBook(inventory);
-            System.out.println( "If you would like to check in a book please press C");
-            System.out.println(" If you want to go Home press X");
-
-            scanner.nextLine();
-        String checkoutOrHome = scanner.nextLine();
-
-            if (checkoutOrHome.equals("C")){
-                System.out.println("Please enter the ID of the book you want to check in: ");
-                int checkInUserId = scanner.nextInt();
-
-                checkInBook(inventory,checkInUserId);
-                showCheckedOutBook(inventory);
-            }
-    }
+//        int userInput = scanner.nextInt();
+//
+//        if (userInput == 1 ) {
+//    int proceedToCheckout = 0;
+//    while (proceedToCheckout != 2) {
+//        showAvailableBooks(inventory);
+//
+//        System.out.println("\n");
+//        System.out.println("Would you like to checkout any of the books above? ");
+//        System.out.println("(Press 1 to proceed to checkout or 2 to exit back to HomeScreen)");
+//
+//        proceedToCheckout = scanner.nextInt();
+//        if (proceedToCheckout == 1) {
+//
+//            System.out.println("Please enter your name for the record: ");
+//            String userName = scanner.nextLine();
+//
+//            System.out.println("Please enter the Id of the book you'd like the checkout: ");
+//            int checkoutBookID = scanner.nextInt();
+//            for (int i = 0; i < inventory.length; i++) {
+//                if (inventory[i].getId() == checkoutBookID) {
+//                    inventory[i].setCheckedOutTo(userName);
+//                    inventory[i].setCheckedOut(true);
+//                    System.out.println("The user " + userName + " has successfully checkout the following book: " + inventory[i].getTitle());
+//
+//                }
+//            }
+//        }
+//    }
+//
+//    }
+//    if (userInput == 2 ){
+//     showCheckedOutBook(inventory);
+//            System.out.println( "If you would like to check in a book please press C");
+//            System.out.println(" If you want to go Home press X");
+//
+//            scanner.nextLine();
+//        String checkoutOrHome = scanner.nextLine();
+//
+//            if (checkoutOrHome.equals("C")){
+//                System.out.println("Please enter the ID of the book you want to check in: ");
+//                int checkInUserId = scanner.nextInt();
+//
+//                checkInBook(inventory,checkInUserId);
+//                showCheckedOutBook(inventory);
+//            }
+//    }
 
 }
 
@@ -127,8 +196,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        displayHomeScreen();
-
+    displayHomeScreen();
 
 
 
