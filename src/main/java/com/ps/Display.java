98c8976123e07616b917c751945743cd29c632ab
Sheik siphat <notsheiksiphat2003@gmail.com>
2025-04-20 18:35:10 -0400

    broke down case 1 into another function called checkOutBook to further organize and clean code

--- src/main/java/com/ps/Display.java
+++ src/main/java/com/ps/Display.java
@@ -1,4 +1,5 @@
 package com.ps;
 
 public class Display {
+
 }
--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -30,12 +30,11 @@ public class Main {
     };
     public static void displayHomeScreen(){
 
-
         while(true){
             mainMenu();
-
             int choice = scanner.nextInt();
             scanner.nextLine();
+
             switch (choice){
                 case 1:
                     int proceedToCheckout = 0;
@@ -56,17 +55,9 @@ public class Main {
                             System.out.println("Please enter the Id of the book you'd like the checkout: ");
                             int checkoutBookID = scanner.nextInt();
                             scanner.nextLine();
-                            for (int i = 0; i < inventory.length; i++) {
-                                if (inventory[i].getId() == checkoutBookID) {
-                                    inventory[i].setCheckedOutTo(userName);
-                                    inventory[i].setCheckedOut(true);
-                                    System.out.println("The user " + userName + " has successfully checkout the following book: " + inventory[i].getTitle());
 
-                                }
-                            }
-
+                            checkOutBook(inventory,checkoutBookID,userName);
                         }
-
                     }
                     break;
                 case 2:
@@ -95,9 +86,6 @@ public class Main {
             }
         }
 
-
-
-
     }
 
     public static void mainMenu(){
@@ -107,64 +95,7 @@ public class Main {
         System.out.println("2) Show Checked out Books");
         System.out.println("3) Exit the application");
 
-//        int userInput = scanner.nextInt();
-//
-//        if (userInput == 1 ) {
-//    int proceedToCheckout = 0;
-//    while (proceedToCheckout != 2) {
-//        showAvailableBooks(inventory);
-//
-//        System.out.println("\n");
-//        System.out.println("Would you like to checkout any of the books above? ");
-//        System.out.println("(Press 1 to proceed to checkout or 2 to exit back to HomeScreen)");
-//
-//        proceedToCheckout = scanner.nextInt();
-//        if (proceedToCheckout == 1) {
-//
-//            System.out.println("Please enter your name for the record: ");
-//            String userName = scanner.nextLine();
-//
-//            System.out.println("Please enter the Id of the book you'd like the checkout: ");
-//            int checkoutBookID = scanner.nextInt();
-//            for (int i = 0; i < inventory.length; i++) {
-//                if (inventory[i].getId() == checkoutBookID) {
-//                    inventory[i].setCheckedOutTo(userName);
-//                    inventory[i].setCheckedOut(true);
-//                    System.out.println("The user " + userName + " has successfully checkout the following book: " + inventory[i].getTitle());
-//
-//                }
-//            }
-//        }
-//    }
-//
-//    }
-//    if (userInput == 2 ){
-//     showCheckedOutBook(inventory);
-//            System.out.println( "If you would like to check in a book please press C");
-//            System.out.println(" If you want to go Home press X");
-//
-//            scanner.nextLine();
-//        String checkoutOrHome = scanner.nextLine();
-//
-//            if (checkoutOrHome.equals("C")){
-//                System.out.println("Please enter the ID of the book you want to check in: ");
-//                int checkInUserId = scanner.nextInt();
-//
-//                checkInBook(inventory,checkInUserId);
-//                showCheckedOutBook(inventory);
-//            }
-//    }
-
 }
-
-//        String userChoice = scanner.nextLine();
-
-        //1 display books avialble
-//        System.out.println("The following Books are available formated in the following |id | ISBN | Title | "  );
-//        showAvailableBooks(inventory);
-
-        //2 show checked out books
-
 
 
     public  static void showAvailableBooks(Book[] inventory){
@@ -174,12 +105,13 @@ public class Main {
                 System.out.println( inventory[i].getId() + " " + inventory[i].getIsbn() + " " + inventory[i].getTitle());
             }
         }
+
     }
 
     public static void showCheckedOutBook(Book[] inventory){
         for (int i = 0; i < inventory.length; i++) {
             if (inventory[i].isCheckedOut() ){
-                System.out.println("This book is checked out : " + inventory[i].getId() + " " + inventory[i].getIsbn()+ " " + inventory[i].getTitle() + "It has been checked out by "+ inventory[i].getCheckedOutTo());
+                System.out.println("This book is checked out : " + inventory[i].getId() + " " + inventory[i].getIsbn()+ " " + inventory[i].getTitle() + "  has been checked out by "+ inventory[i].getCheckedOutTo());
 
 
             }
@@ -192,13 +124,21 @@ public class Main {
                 inventory[i].setCheckedOut(false);
             }
         }
-    };
+    }
+
+    public static void checkOutBook(Book[] inventory,int checkoutBookId, String userName ){
+        for (int i = 0; i < inventory.length; i++) {
+            if (inventory[i].getId() == checkoutBookId) {
+                inventory[i].setCheckedOutTo(userName);
+                inventory[i].setCheckedOut(true);
+                System.out.println("The user " + userName + " has successfully checkout the following book: " + inventory[i].getTitle());
+            }
+        }
+    }
 
     public static void main(String[] args) {
 
     displayHomeScreen();
-
-
 
     }
 }
